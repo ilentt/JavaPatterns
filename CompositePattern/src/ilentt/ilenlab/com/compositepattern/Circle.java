3a3faa2d31203dd19d74e7e5c3b50d70a1ee83fb
ilentt <ilentt@ilenlab.com>
2015-08-21 14:21:36 +0700

    update composite pattern

---
+++ CompositePattern/src/ilentt/ilenlab/com/compositepattern/Circle.java
@@ -0,0 +1,7 @@
+package ilentt.ilenlab.com.compositepattern;
+
+public class Circle implements Shape {
+	public void Draw(String fillColor) {
+		System.out.println("Draw circle with color: " + fillColor);
+	}
+}
---
+++ CompositePattern/src/ilentt/ilenlab/com/compositepattern/Shape.java
@@ -0,0 +1,5 @@
+package ilentt.ilenlab.com.compositepattern;
+
+public interface Shape {
+	public void Draw(String fillColor);
+}
